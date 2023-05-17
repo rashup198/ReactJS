d57739f1513c350c3c2b59bc09464bfefdc2563e
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-17 18:44:12 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./App.css";
+import Item from "./components/Item"; 
+
+function App() {
+  return (
+    <div>
+      <Item></Item>
+      <div className ="App">Hello Jii</div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,7 @@
+import './Item.css';
+
+function Item(){
+    return (<p className='Nirma'>Nirma</p>);
+}
+
+export default Item;
