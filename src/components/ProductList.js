e80436d6b6cf6993514970e3ae46b942004612b3
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-23 18:57:53 +0530

    updated

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,10 @@
-
+import react from 'react'
 import Product from './Product'
 export default function ProductList(props) {
 
   return (
-   
-       props.productList.map((product)=>{
-        return <product product={product}/>
+       props.productList.map((product,i)=>{
+        return <Product product={product} key={i}/>
        }
     
   )
